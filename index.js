41c233d89f3cc7ad26754a2f252427b0f4c2a6fb
Alberto Fernandez Reyes <albertofernandezreyes@gmail.com>
2019-04-04 23:17:06 +0300

    TDD and implementing methods.

--- index.js
+++ index.js
@@ -15,26 +15,83 @@ module.exports = class Arboleda {
   }
 
   isRoot () {
-    return this.getParent() === null
+    return this.getNode() !== null && this.getParent() === null
   }
 
   isLeave () {
-    return this.getChildren().length === 0
+    return this.getNode() && this.getChildren().length === 0
   }
 
-  setContent (content = {}) {
+  setNode (content) {
+    if(content == null || content == undefined) {
+      throw new Error('#setNode parameter: Expect non-null nor undefined parameter')
+    }
+
     this._content = content
+    return this
   }
 
-  getContent () {
+  getNode () {
     return this._content
   }
 
-  setParent (node) {
-    if (!(node instanceof Arboleda)) {
+  addChild(arboledaInstance) {
+    if(arboledaInstance instanceof Arboleda) {
+      this._children.push(arboledaInstance);
+      
+      arboledaInstance.setParent(this)
+
+      return this
+    }
+
+    throw new Error('#addChild parameter: Expect non-null arboledaInstance')
+  }
+
+  hasNextSibling(){
+    if(!this.getParent()) {
+      return false      
+    }
+
+    const children = this.getParent().getChildren()
+    if(children.length == 0) {
+      return false
+    }
+
+    const index = children.indexOf(this)
+
+    // Check if it is the last child
+    return (index !== children.length-1)
+  }
+
+  getNextSibling(){
+    if(!this.getParent()) {
+      return null      
+    }
+
+    const children = this.getParent().getChildren()
+    if(children.length == 0) {
+      return null
+    }
+
+    let index = children.indexOf(this)
+
+    // Check if it is the last child
+    if(index == children.length-1) {
+      return null
+    }
+
+    return children[index+1]
+  }
+
+  setParent (parentNode) {
+    if (!(parentNode instanceof Arboleda)) {
       throw new Error('Parent MUST BE instance of Arboleda')
     }
 
-    this._parent = node
+    if(!parentNode.getChildren().includes(this)) {
+      parentNode.addChild(this);
+    }
+
+    this._parent = parentNode
   }
 }
--- test/arboleda.spec.js
+++ test/arboleda.spec.js
@@ -23,91 +23,166 @@ describe('Arboleda', function () {
       })
 
       it('should return false if it is not a root node', function () {
+        const rootArbol = new Arboleda()
+        rootArbol.setNode({}).addChild(arbol)
         expect(arbol.isRoot()).to.not.be.ok()
       })
 
       it('should return true if it is a root node', function () {
-        expect(arbol).isRoot()
+        const rootArbol = new Arboleda()
+        rootArbol.setNode({})
+        expect(rootArbol.isRoot()).to.be.ok()
       })
     })
 
     describe('#isLeave()', function () {
-      it('should return true if it has no children', function () {
-        expect(false).to.be.ok()
+      it('should return false if it has children', function () {
+        const rootArbol = new Arboleda()
+        rootArbol.setNode({})
+        
+        const leafArbol = new Arboleda()
+        leafArbol.setNode({})
+
+        rootArbol.addChild(leafArbol)
+        
+        expect(rootArbol.isLeave()).to.not.be.ok()
       })
 
-      it('should return false if it has children', function () {
-        expect(false).to.be.ok()
+      it('should return false if it has no node data', function () {
+        const rootArbol = new Arboleda()
+        rootArbol.setNode({})
+        
+        const leafArbol = new Arboleda()
+
+        rootArbol.addChild(leafArbol)
+        
+        expect(rootArbol.isLeave()).to.not.be.ok()
+      })
+
+      it('should return true if it has no children', function () {
+        const rootArbol = new Arboleda()
+        rootArbol.setNode({})
+        
+        const leafArbol = new Arboleda()
+        leafArbol.setNode({})
+
+        rootArbol.addChild(leafArbol)
+        
+        expect(leafArbol.isLeave()).to.be.ok()
       })
     })
 
     describe('#setNode()', function () {
       it('should set the node content', function () {
-        expect(false).to.be.ok()
+        const content = {}
+        const rootArbol = new Arboleda()
+        rootArbol.setNode(content)
+
+        expect(rootArbol.getNode()).to.equal(content)
       })
 
       it('should throw error if node is set to null', function () {
-        expect(false).to.be.ok()
+        const content = {}
+        const rootArbol = new Arboleda()
+        expect(rootArbol.setNode).withArgs(null).to.throwException()
       })
 
       it('should throw error if node is set to undefined', function () {
-        expect(false).to.be.ok()
+        const content = {}
+        const rootArbol = new Arboleda()
+        expect(rootArbol.setNode).withArgs(undefined).to.throwException()
       })
     })
 
     describe('#getNode()', function () {
       it('should return null if node has not been set', function () {
-        expect(false).to.be.ok()
+        const rootArbol = new Arboleda()
+        expect(rootArbol.getNode()).to.be.equal(null)
       })
 
       it('should return node contents', function () {
-        expect(false).to.be.ok()
+        const content = {}
+        const rootArbol = new Arboleda(content)
+        expect(rootArbol.getNode()).to.equal(content)
       })
     })
 
     describe('#setParent()', function () {
+      it('should return throw if undefined is passed as parent node', function () {
+        const rootArbol = new Arboleda({})
+        expect(rootArbol.setParent).withArgs().to.throwException()
+      })
+
+      it('should return throw if null is passed as parent node', function () {
+        const rootArbol = new Arboleda({})
+        expect(rootArbol.setParent).withArgs(null).to.throwException()
+      })
+
+      it('should return throw if non-arboleda instance is passed as parent node', function () {
+        const rootArbol = new Arboleda({})
+        expect(rootArbol.setParent).withArgs('hello').to.throwException()
+        expect(rootArbol.setParent).withArgs({}).to.throwException()
+      })
+
       it('should return node having as a parent the passed node', function () {
-        expect(false).to.be.ok()
+        const parentArbol = new Arboleda({})
+        const childArbol = new Arboleda({})
+
+        childArbol.setParent(parentArbol)
+        expect(childArbol.getParent()).to.equal(parentArbol)
+        expect(parentArbol.getChildren()).to.contain(childArbol)
       })
     })
 
     describe('#hasNextSibling()', function () {
-      it('should return true if node has a sibiling on his right side', function () {
-        expect(false).to.be.ok()
+      it('should return true if node has a sibling on his right side', function () {
+        const parentArbol = new Arboleda({})
+        const childA = new Arboleda({})
+        const childB = new Arboleda({})
+
+        parentArbol.addChild(childA)
+        parentArbol.addChild(childB)
+
+        expect(childA.hasNextSibling()).to.be.ok()
       })
 
-      it('should return false if node does not have a sibiling on his right side', function () {
-        expect(false).to.be.ok()
-      })
-    })
+      it('should return false if node does not have a sibling on his right side', function () {
+        const parentArbol = new Arboleda({})
+        expect(parentArbol.hasNextSibling()).to.not.be.ok()
 
-    describe('#hasPrevSibling()', function () {
-      it('should return true if node has a sibiling on his left side', function () {
-        expect(false).to.be.ok()
-      })
+        const childA = new Arboleda({})
+        const childB = new Arboleda({})
 
-      it('should return false if node does not have a sibiling on his left side', function () {
-        expect(false).to.be.ok()
+        parentArbol.addChild(childA)
+        parentArbol.addChild(childB)
+
+        expect(childB.hasNextSibling()).to.not.be.ok()
       })
     })
 
     describe('#getNextSibling()', function () {
       it('should return null if node does not have a right sibling', function () {
-        expect(false).to.be.ok()
+        const parentArbol = new Arboleda({})
+        expect(parentArbol.getNextSibling()).to.be(null)
+
+        const childA = new Arboleda({})
+        const childB = new Arboleda({})
+
+        parentArbol.addChild(childA)
+        parentArbol.addChild(childB)
+
+        expect(childB.getNextSibling()).to.be(null)
       })
 
       it('should return right sibling of the node', function () {
-        expect(false).to.be.ok()
-      })
-    })
+        const parentArbol = new Arboleda({})
+        const childA = new Arboleda({})
+        const childB = new Arboleda({})
 
-    describe('#getPrevSibling()', function () {
-      it('should return null if node does not have a left sibling', function () {
-        expect(false).to.be.ok()
-      })
+        parentArbol.addChild(childA)
+        parentArbol.addChild(childB)
 
-      it('should return left sibling of the node', function () {
-        expect(false).to.be.ok()
+        expect(childA.getNextSibling()).to.equal(childB)
       })
     })
 
